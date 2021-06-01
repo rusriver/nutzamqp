37658671d9d7162c126e9066aeb069e2819e60c1
Leonid Titov <ltmolprod@gmail.com>
2021-06-01 13:58:06 +0300

    20210601-135805 {a515a7da602b3ae39177e9de11e2d77570cf5845}

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/rusriver/nutzamqp
 
 go 1.16
+
+require (
+	github.com/rusriver/config v0.0.0-20210528100655-4e62ef328576
+	github.com/streadway/amqp v1.0.0
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- nutzamqp.go
+++ nutzamqp.go
@@ -1,3 +1,64 @@
 package nutzamqp
 
-import ()
+import (
+	"fmt"
+
+	"github.com/rusriver/config"
+	"github.com/streadway/amqp"
+)
+
+func AMQPBatchDeclare(
+	channel *amqp.Channel,
+	cfg *config.Config,
+) {
+	var err error
+
+	for k, _ := range cfg.UMap("exchanges") {
+		x, err := cfg.Get("exchanges." + k)
+		if err != nil {
+			panic(fmt.Errorf("!!! nutzamqp.go:18 / 1.1 at \"x, err := cfg.Get(\"exchanges.\" + k)\": %v", err))
+		}
+		err = channel.ExchangeDeclare(
+			x.UString("name"),      // name
+			x.UString("type"),      // type
+			x.UBool("durable"),     // durable
+			x.UBool("autodeleted"), // auto-deleted
+			false,                  // internal
+			false,                  // noWait
+			nil,                    // arguments
+		)
+		if err != nil {
+			panic(fmt.Errorf("!!! nutzamqp.go:27 / 1.2: %v", err))
+		}
+	}
+	for k, _ := range cfg.UMap("queues") {
+		q, err := cfg.Get("queues." + k)
+		if err != nil {
+			panic(fmt.Errorf("!!! nutzamqp.go:32 / 2.1 at \"q, err := cfg.Get(\"queues.\" + k)\": %v", err))
+		}
+		_, err = channel.QueueDeclare(
+			q.UString("name"),  // name of the queue
+			q.UBool("durable"), // durable
+			false,              // delete when unused
+			false,              // exclusive
+			false,              // noWait
+			nil,                // arguments
+		)
+		if err != nil {
+			panic(fmt.Errorf("!!! nutzamqp.go:40 / 2.2: %v", err))
+		}
+	}
+	for _, v := range cfg.UMap("bindings") {
+		b := &config.Config{Root: v}
+		err = channel.QueueBind(
+			b.UString("2"), // name of the queue
+			b.UString("1"), // bindingKey
+			b.UString("0"), // sourceExchange
+			false,          // noWait
+			nil,            // arguments
+		)
+		if err != nil {
+			panic(fmt.Errorf("!!! nutzamqp.go:52 / 3.2: %v", err))
+		}
+	}
+}
