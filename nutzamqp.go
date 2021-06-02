5819095957a0373a88eadccd10f8b8427487bb99
Leonid Titov <ltmolprod@gmail.com>
2021-06-02 23:22:36 +0300

    20210602-232235 {2cf68b6675ace4594914df9d94313a15947be999}

--- nutzamqp.go
+++ nutzamqp.go
@@ -2,7 +2,6 @@ package nutzamqp
 
 import (
 	"fmt"
-	"regexp"
 
 	"github.com/rusriver/config"
 	"github.com/rusriver/filtertag"
@@ -16,26 +15,15 @@ func AMQPBatchDeclare(
 ) {
 	var err error
 
-	re_ddash_dot, err := regexp.Compile("--")
-	if err != nil {
-		panic(fmt.Errorf("!!! nutzamqp.go:19 / 0.1 at \"re_ddash_dot, err := regexp.Compile(\"--\")\": %v", err))
-	}
-
 	log.Fields["lib"] = "nutzamqp.AMQPBatchDeclare"
 
-	for k, _ := range cfg.UMap("exchanges") {
+	for _, v := range cfg.UList("exchanges") {
+		x := &config.Config{Root: v}
 
-		log.Info("exchange declare: %v", k)
-
-		x, err := cfg.Get("exchanges." + k)
-		if err != nil {
-			panic(fmt.Errorf("!!! nutzamqp.go:28 / 1.1 at \"x, err := cfg.Get(\"exchanges.\" + k)\": %v", err))
-		}
-
-		k = re_ddash_dot.ReplaceAllString(k, ".")
+		log.Info("exchange declare: %v", x.UString("name"))
 
 		err = channel.ExchangeDeclare(
-			k,                      // name
+			x.UString("name"),      // name
 			x.UString("type"),      // type
 			x.UBool("durable"),     // durable
 			x.UBool("autodeleted"), // auto-deleted
@@ -44,24 +32,18 @@ func AMQPBatchDeclare(
 			nil,                    // arguments
 		)
 		if err != nil {
-			panic(fmt.Errorf("!!! nutzamqp.go:40 / 1.2: %v", err))
+			panic(fmt.Errorf("!!! nutzamqp.go:34 / 1.2: %v", err))
 		}
 
-		log.Info("exchange declared OK: %v", k)
+		log.Info("exchange declared OK: %v", x.UString("name"))
 	}
-	for k, _ := range cfg.UMap("queues") {
+	for _, v := range cfg.UList("queues") {
+		q := &config.Config{Root: v}
 
-		log.Info("queue declare: %v", k)
-
-		q, err := cfg.Get("queues." + k)
-		if err != nil {
-			panic(fmt.Errorf("!!! nutzamqp.go:50 / 2.1 at \"q, err := cfg.Get(\"queues.\" + k)\": %v", err))
-		}
-
-		k = re_ddash_dot.ReplaceAllString(k, ".")
+		log.Info("queue declare: %v", q.UString("name"))
 
 		_, err = channel.QueueDeclare(
-			k,                             // name of the queue
+			q.UString("name"),             // name of the queue
 			q.UBool("durable"),            // durable
 			q.UBool("delete_when_unused"), // delete when unused
 			false,                         // exclusive
@@ -69,10 +51,10 @@ func AMQPBatchDeclare(
 			nil,                           // arguments
 		)
 		if err != nil {
-			panic(fmt.Errorf("!!! nutzamqp.go:61 / 2.2: %v", err))
+			panic(fmt.Errorf("!!! nutzamqp.go:52 / 2.2: %v", err))
 		}
 
-		log.Info("queue declared OK: %v", k)
+		log.Info("queue declared OK: %v", q.UString("name"))
 	}
 	for _, v := range cfg.UList("bindings") {
 		b := &config.Config{Root: v}
@@ -89,7 +71,7 @@ func AMQPBatchDeclare(
 			nil,   // arguments
 		)
 		if err != nil {
-			panic(fmt.Errorf("!!! nutzamqp.go:80 / 3.2: %v", err))
+			panic(fmt.Errorf("!!! nutzamqp.go:71 / 3.2: %v", err))
 		}
 
 		log.Info("binding declared OK (x--k--q): '%v' -- '%v' -- '%v'", x, k, q)
