656fc9245fcb42045617f0d675900a603e58c746
Leonid Titov <ltmolprod@gmail.com>
2021-06-02 21:43:14 +0300

    20210602-214313 {51686e79844eda565a1faf7befea50139c26f4e5}

--- nutzamqp.go
+++ nutzamqp.go
@@ -10,84 +10,67 @@ import (
 )
 
 func AMQPBatchDeclare(
-	log *filtertag.Entry,
-	channel *amqp.Channel,
-	cfg *config.Config,
+	log	*filtertag.Entry,
+	channel	*amqp.Channel,
+	cfg	*config.Config,
 ) {
 	var err error
-
-	re_ddash_dot, err := regexp.Compile(`--`)
-	if err != nil {
-		panic(fmt.Errorf("!!! nutzamqp.go:19 / 0.1 at \"re_ddash_dot, err := regexp.Compile(`--`)\": %v", err))
-	}
+	
+	re_ddash_dot, err := regexp.Compile("\-\-"); if err != nil { panic(fmt.Errorf("!!! nutzamqp.go:19 / 0.1 at \"re_ddash_dot, err := regexp.Compile(\"\-\-\")\": %v", err)) }
 
 	log.Fields["lib"] = "nutzamqp.AMQPBatchDeclare"
 
 	for k, _ := range cfg.UMap("exchanges") {
+	
+		x, err := cfg.Get("exchanges." + k); if err != nil { panic(fmt.Errorf("!!! nutzamqp.go:26 / 1.1 at \"x, err := cfg.Get(\"exchanges.\" + k)\": %v", err)) }
 
-		x, err := cfg.Get("exchanges." + k)
-		if err != nil {
-			panic(fmt.Errorf("!!! nutzamqp.go:26 / 1.1 at \"x, err := cfg.Get(\"exchanges.\" + k)\": %v", err))
-		}
-
-		k = re_ddash_dot.ReplaceAllString(k, ".")
-
+		k = re_ddash_dot.ReplaceAllString( k, ".")
+	
 		err = channel.ExchangeDeclare(
-			k,                      // name
-			x.UString("type"),      // type
-			x.UBool("durable"),     // durable
-			x.UBool("autodeleted"), // auto-deleted
-			false,                  // internal
-			false,                  // noWait
-			nil,                    // arguments
-		)
-		if err != nil {
-			panic(fmt.Errorf("!!! nutzamqp.go:38 / 1.2: %v", err))
-		}
-
+			k,		// name
+			x.UString	("type"),	// type
+			x.UBool	("durable"),	// durable
+			x.UBool	("autodeleted"),	// auto-deleted
+			false,		// internal
+			false,		// noWait
+			nil,		// arguments
+		); if err != nil { panic(fmt.Errorf("!!! nutzamqp.go:38 / 1.2: %v", err)) }
+		
 		log.Info("exchange declared OK: %v", k)
 	}
 	for k, _ := range cfg.UMap("queues") {
 
-		q, err := cfg.Get("queues." + k)
-		if err != nil {
-			panic(fmt.Errorf("!!! nutzamqp.go:46 / 2.1 at \"q, err := cfg.Get(\"queues.\" + k)\": %v", err))
-		}
+		q, err := cfg.Get("queues." + k); if err != nil { panic(fmt.Errorf("!!! nutzamqp.go:46 / 2.1 at \"q, err := cfg.Get(\"queues.\" + k)\": %v", err)) }
 
-		k = re_ddash_dot.ReplaceAllString(k, ".")
+		k = re_ddash_dot.ReplaceAllString( k, ".")
 
 		_, err = channel.QueueDeclare(
-			k,                             // name of the queue
-			q.UBool("durable"),            // durable
-			q.UBool("delete_when_unused"), // delete when unused
-			false,                         // exclusive
-			false,                         // noWait
-			nil,                           // arguments
-		)
-		if err != nil {
-			panic(fmt.Errorf("!!! nutzamqp.go:57 / 2.2: %v", err))
-		}
-
+			k,		// name of the queue
+			q.UBool	("durable"),	// durable
+			q.UBool	("delete_when_unused"),	// delete when unused
+			false,		// exclusive
+			false,		// noWait
+			nil,		// arguments
+		); if err != nil { panic(fmt.Errorf("!!! nutzamqp.go:57 / 2.2: %v", err)) }
+		
 		log.Info("queue declared OK: %v", k)
 	}
 	for _, v := range cfg.UList("bindings") {
-		b := &config.Config{Root: v}
-
+		b := &config.Config{ Root: v }
+		
 		x := b.UString("0")
 		k := b.UString("1")
 		q := b.UString("2")
-
+		
 		err = channel.QueueBind(
-			q,     // name of the queue
-			k,     // bindingKey
-			x,     // sourceExchange
-			false, // noWait
-			nil,   // arguments
-		)
-		if err != nil {
-			panic(fmt.Errorf("!!! nutzamqp.go:76 / 3.2: %v", err))
-		}
-
+			q,	// name of the queue
+			k,	// bindingKey
+			x,	// sourceExchange
+			false,	// noWait
+			nil,	// arguments
+		); if err != nil { panic(fmt.Errorf("!!! nutzamqp.go:76 / 3.2: %v", err)) }
+		
 		log.Info("binding declared OK (x--k--q): '%v' -- '%v' -- '%v'", x, k, q)
 	}
 }
+
